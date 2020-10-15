e1435017ad158ab3ad58740ab0a1b39a93ad5a61
Brodrick George <brodrick.george@yahoo.com>
2020-10-15 00:14:26 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,11 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => ["Lord Montague"]
+        :name => ["Lord Montague"],
         :age => ["35"]
         },
       :matriarch => {
-        #your key/value pairs here
+       :name => ["Lady Montague"],
+       :age => ["54"]
         },
       :hero => {
         #your key/value pairs here
