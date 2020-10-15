aafc25d421204789e2bbb5f713e7f2211b55efdc
Brodrick George <brodrick.george@yahoo.com>
2020-10-15 00:16:50 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -18,11 +18,12 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        
+        :name => ["Lord Capulet"],
+        :age => ["50"]
 
         },
       :matriarch => {
-        #your key/value pairs here
+        
         },
       :heroine => {
         #your key/value pairs here
